2dddd02ba6cf20de4f27c190e38def1f171ae655
Jobin Muthalaly <jobin@thisisops.com>
2024-11-09 19:01:48 -0800

    Add Sprite groups

--- main.py
+++ main.py
@@ -13,18 +13,25 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.draw(screen)
-        pygame.display.flip()
         fps = game_clock.tick(60)
         dt = fps / 1000
+        screen.fill("black")
 
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
